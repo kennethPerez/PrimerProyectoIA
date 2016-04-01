16f1670a9d71727d022bfc821f6d357764617e66
Kenneth Perez <kenn.perez@gmail.com>
2016-03-31 22:57:31 -0600

    all proyect

--- IA/index.aspx.designer.cs
+++ IA/index.aspx.designer.cs
@@ -76,6 +76,15 @@ namespace IA {
         protected global::System.Web.UI.WebControls.Button buttonTwitter;
         
         /// <summary>
+        /// button1 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button button1;
+        
+        /// <summary>
         /// FileUpload control.
         /// </summary>
         /// <remarks>
